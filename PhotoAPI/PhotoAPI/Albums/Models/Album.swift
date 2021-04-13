477d6141ef695997a4ed2d814de227d2f3c591f1
Rodrigo Candido <rodrigoipu@hotmail.com>
2021-04-13 19:43:07 +0200

    On createInitialStructure - Create first and simple folder structure Analyse JSON response to be able to create base models Analyse JSON responde and request to create Network layer Create simple Albums and Photos structures

---
+++ PhotoAPI/PhotoAPI/Albums/Models/Album.swift
@@ -0,0 +1,14 @@
+//
+//  Album.swift
+//  PhotoAPI
+//
+//  Created by Rodrigo  Candido on 13/4/21.
+//
+
+import Foundation
+
+struct Album {
+    let id:     Int
+    let userId: Int
+    let title:  String
+}
---
+++ PhotoAPI/PhotoAPI/Photos/Models/Photos.swift
@@ -0,0 +1,16 @@
+//
+//  Photos.swift
+//  PhotoAPI
+//
+//  Created by Rodrigo  Candido on 13/4/21.
+//
+
+import Foundation
+struct Photos {
+    
+    let id:           Int
+    let albumId:      Int
+    let title:        String
+    let url:          String
+    let thumbNailUrl: String
+}
